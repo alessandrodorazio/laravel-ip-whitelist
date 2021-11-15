7922312790336c6a6f81c009e3ad7eb1343b1176
alessandrodorazio <alessandrodorazio@users.noreply.github.com>
2021-11-15 11:04:29 +0000

    Fix styling

--- src/Http/Middleware/IpWhitelist.php
+++ src/Http/Middleware/IpWhitelist.php
@@ -5,18 +5,18 @@ namespace Alessandrodorazio\LaravelIpWhitelist\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Redirect;
 
 class IpWhitelist
 {
-
     public function handle(Request $request, Closure $next)
     {
         $ipAddresses = config('ip-whitelist.whitelist');
-        if(!in_array($request->ip(), $ipAddresses) && !config('app.debug')) {
+        if (! in_array($request->ip(), $ipAddresses) && ! config('app.debug')) {
             Log::error('IP address not in whitelist ' . $request->ip());
+
             return response()->json('IP not allowed');
         }
+
         return $next($request);
     }
 }
